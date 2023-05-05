61dc13f9c8279230f3b3b059c39ca6d413edcd38
Lazar Stojanović <lakisd98@gmail.com>
2023-05-05 16:58:24 +0200

    Add: IP search box

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import Display from "./components/Display";
 function App() {
 	return (
 		<>
-			<section className="bg-mobile-pattern">
+			<section className="bg-mobile-pattern bg-cover px-6">
 				<h1 className="text-2xl text-white font-rubik font-medium text-center py-6">IP Address Tracker</h1>
+				<Search />
 			</section>
-			<Search />
 		</>
 	);
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,18 @@
+import arrowSvg from "../assets/images/icon-arrow.svg";
+
 const Search = () => {
-	return <></>;
+	return (
+		<form action="" className="flex">
+			<input
+				type="text"
+				placeholder="Search for any IP address or domain"
+				className="py-4 rounded-l-xl pl-5 w-full placeholder:text-sm"
+			/>
+			<button className="bg-black rounded-r-xl p-5">
+				<img src={arrowSvg} alt="confirm button" />
+			</button>
+		</form>
+	);
 };
 
 export default Search;
