ee55f2c10c2765228f78dd057bd3d160041f3cb1
Lazar Stojanović <lakisd98@gmail.com>
2023-05-06 16:13:40 +0200

    Update: main box position

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,27 +1,29 @@
 const Display = (props) => {
 	return (
-		<>
-			<section className="text-black text-center bg-white p-5 rounded-xl absolute z-10">
+		<div className="flex justify-center">
+			<section className="text-black text-center bg-white p-5 rounded-xl absolute mt-7 w-[85%]">
 				<div className="mb-4">
 					<p className="text-xs text-darkGray tracking-wider mb-1">IP ADDRESS</p>
-					<p className="text-3xl font-bold">{props.location.IP}</p>
+					<p className="text-xl font-bold">{props.location.IP}</p>
 				</div>
 				<div className="mb-4">
 					<p className="text-xs text-darkGray tracking-wider mb-1">LOCATION</p>
-					<p className="text-3xl font-bold">
-						{props.location.location}, {props.location.country} {props.location.zip}
+					<p className="text-xl font-bold">
+						{props.location.location
+							? `${props.location.location}, ${props.location.country} ${props.location.zip}`
+							: ""}
 					</p>
 				</div>
 				<div className="mb-4">
 					<p className="text-xs text-darkGray tracking-wider mb-1">TIMEZONE</p>
-					<p className="text-3xl font-bold">UTC {props.location.timezone}</p>
+					<p className="text-xl font-bold">{props.location.timezone ? `UTC ${props.location.timezone}` : ""}</p>
 				</div>
 				<div>
 					<p className="text-xs text-darkGray tracking-wider mb-1">ISP</p>
-					<p className="text-3xl font-bold">{props.location.isp}</p>
+					<p className="text-xl font-bold">{props.location.isp}</p>
 				</div>
 			</section>
-		</>
+		</div>
 	);
 };
 
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -12,7 +12,14 @@ const Map = (props) => {
 	}, [props.coords]);
 
 	return (
-		<MapContainer ref={mapRef} center={props.coords} zoom={10} scrollWheelZoom={true} className="h-[65%] w-screen">
+		<MapContainer
+			ref={mapRef}
+			center={props.coords}
+			zoom={10}
+			scrollWheelZoom={false}
+			doubleClickZoom={false}
+			zoomControl={false}
+			className="h-[65%] w-screen absolute -z-10">
 			<TileLayer
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
